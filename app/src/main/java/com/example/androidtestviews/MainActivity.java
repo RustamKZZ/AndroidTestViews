1b738a0b60259da168d361d7fe4cccce7a244805
RustamKZZ <kzrustam@gmail.com>
2021-09-10 23:12:56 +0300

    Homework01

--- app/src/main/java/com/example/androidtestviews/MainActivity.java
+++ app/src/main/java/com/example/androidtestviews/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.androidtestviews;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements onClickListener, View.OnClickListener {
+    EditText textED;
+    Button btnSave;
+    Button btnCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textED = findViewById(R.id.text_ed);
+        btnSave = findViewById(R.id.btn_save);
+        btnCancel = findViewById(R.id.btn_cancel);
+
+        textED.setOnClickListener(this);
+        btnSave.setOnClickListener(this);
+        btnCancel.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btn_save) {
+            textED.setText("Сохранено");
+        } else if (view.getId() == R.id.btn_cancel) {
+            {
+                textED.setText("Отмена");
+
+            }
+        }
+
     }
 }
